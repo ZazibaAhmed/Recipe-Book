236a5c7af734c9b5b1490456a00207d111b08021
Zaziba Ahmed <54762652+ZazibaAhmed@users.noreply.github.com>
2021-01-24 14:18:58 +0600

    Basic Routing Done

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -12,7 +13,9 @@ export class RecipeListComponent implements OnInit {
   //@Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] ;
 
-  constructor(private recipeService: RecipeService) { }
+  constructor(private recipeService: RecipeService, 
+              private route: ActivatedRoute, 
+              private router: Router) { }
 
   ngOnInit(): void {
     this.recipes=this.recipeService.getRecipes();
@@ -21,5 +24,9 @@ export class RecipeListComponent implements OnInit {
   // onRecipeSelected(recipe: Recipe) {
   //   this.recipeWasSelected.emit(recipe);
   // }
+
+  onNewRecipe(){
+    this.router.navigate(['new'], {relativeTo: this.route})
+  }
 
 }
